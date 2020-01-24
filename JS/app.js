d0957cc26c6faf79a715d5c3160ef88849c769ba
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-01-24 10:44:29 -0600

    create instances of the player class import

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,13 @@
-import Player from "player.js";
+import { Player } from "./player.js";
+
+let playerX = new Player("playerX", "X");
+
+console.log(playerX.name);
+
+console.log(playerX.token);
+
+let playerO = new Player("playerO", "O");
+
+console.log(playerO.name);
+
+console.log(playerO.token);
