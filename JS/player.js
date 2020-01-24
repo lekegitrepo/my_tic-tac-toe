09376c64f7d63a37455cdcc71e8a3586f83e9644
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-01-24 10:45:14 -0600

    update player class

--- JS/player.js
+++ JS/player.js
@@ -1,14 +1,28 @@
 export class Player {
   constructor(name, token) {
-    this.name = name;
-    this.token = token;
+    this._name = name;
+    this._token = token;
+    this._gender = 'male';
   }
 
   get name() {
-    return this.name;
+    return this._name;
+  }
+
+  set name(nam){
+    return this._name = nam;
+  }
+
+  set gender(mf){
+    return this._gender = mf;
   }
 
   get token() {
-    return this.token;
+    return this._token;
+  }
+
+  set token(token){
+    return this._token = token;
   }
 }
+
