17b966de64f8368f30ece6980c712bea1d994ab7
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-01-24 15:36:52 -0600

    import new class

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,15 @@
 import { Player } from "./player.js";
+import { Board } from './board.js';
+import { GameManager } from './game.js';
+import { UI } from './UI.js';
+
+let board = new Board();
+board.setBoardTile(3, 'X');
+board.setBoardTile(0, 'X');
+
+console.log(board.board);
+
+console.log(board.resetBoard());
 
 let playerX = new Player("playerX", "X");
 
@@ -11,3 +22,8 @@ let playerO = new Player("playerO", "O");
 console.log(playerO.name);
 
 console.log(playerO.token);
+
+let gameManager = new GameManager(playerO, playerX);
+console.log(gameManager.checkWinToken('X'));
+gameManager.winner('O');
+gameManager.winner('X');
